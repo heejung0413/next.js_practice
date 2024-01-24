5b7a46892200f7a20956cc82d66ad44152109533
heejung0413 <acb4287@gmail.com>
2024-01-24 23:28:14 +0900

    temp

--- app/users/[id]/photos/[photosId]/page.tsx
+++ app/users/[id]/photos/[photosId]/page.tsx
@@ -5,7 +5,7 @@ interface Props {
 const PhotoPage = (props: Props) => {
   return (
     <div>
-      photopage {props.params.id} {props.params.photoId}
+      photopage- {props.params.id} {props.params.photoId}
     </div>
   );
 };
